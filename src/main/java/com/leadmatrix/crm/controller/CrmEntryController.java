d12986de99a8ce76059cd095181ccfe5d52a93be
Kartik <tiwariachytum26@gmail.com>
2026-03-26 00:42:05 +0530

    fixed backend and postgres setup

--- src/main/java/com/leadmatrix/crm/controller/CrmEntryController.java
+++ src/main/java/com/leadmatrix/crm/controller/CrmEntryController.java
@@ -1,6 +1,7 @@
 package com.leadmatrix.crm.controller;
 
 import com.leadmatrix.crm.dpo.LoginRequest;
+import com.leadmatrix.crm.dpo.LoginResponse;
 import com.leadmatrix.crm.entity.databaseCRM;
 import com.leadmatrix.crm.respository.crmRespository;
 import com.leadmatrix.crm.security.JwtUtility;
@@ -26,7 +27,7 @@ public class CrmEntryController {
         return "done";
     }
 
-       @Autowired
+      @Autowired
        private  crmRespository crmRespository;
 
        @Autowired
@@ -38,15 +39,19 @@ public class CrmEntryController {
           return "User Registered Successfully";
       }
 
-     //Login API
-     //@PostMapping("/login")
-     //public String loginUser(@RequestBody LoginRequest request){
-       //  return CrmService.loginUser(request.getEmail(), request.getPassword());
-     //}
+
      private final AuthenticationManager authenticationManager;
     private final JwtUtility jwtUtil;
+   // private final crmRespository crmRespository;
 
-    public CrmEntryController(AuthenticationManager authenticationManager, JwtUtility jwtUtil) {
+    public CrmEntryController(AuthenticationManager authenticationManager,
+                              JwtUtility jwtUtil,
+                              crmRespository crmRespository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.crmRespository = crmRespository;
+    }
+    /*public CrmEntryController(AuthenticationManager authenticationManager, JwtUtility jwtUtil) {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
@@ -59,8 +64,23 @@ public class CrmEntryController {
 
         String token = jwtUtil.generateToken(user.getEmail());
         return "Login Successful. Token: " + token;
-     }
+     }*/
 
 
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        databaseCRM user = crmRespository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String token = jwtUtil.generateToken(user.getEmail());
+
+        return new LoginResponse(token, user.getRole(), user.getEmail());
     }
+
+}
 
