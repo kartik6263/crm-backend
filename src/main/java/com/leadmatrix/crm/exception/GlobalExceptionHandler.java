41a6f78220594d05dfc967b036746519cbaa4ac6
Kartik <tiwariachytum26@gmail.com>
2026-03-30 20:17:25 +0530

    Return 401 for bad credentials

--- src/main/java/com/leadmatrix/crm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leadmatrix/crm/exception/GlobalExceptionHandler.java
@@ -1,21 +1,27 @@
 package com.leadmatrix.crm.exception;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentials(BadCredentialsException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("Invalid email or password");
+    }
 
-    public ResponseEntity<String> handleException(Exception e){
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
         e.printStackTrace();
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Something went wrong" + e.getMessage());
-
+                .body("Something went wrong: " + e.getMessage());
     }
-
 }
